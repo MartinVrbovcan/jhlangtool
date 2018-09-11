94890f5b77eec78fe02b594c46948a59bf015237
MartinVrbovcan <martin.vrbovcan@gmail.com>
2018-09-11 22:24:01 +0200

    Fixed setup.py and added url

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ print(path)
 with open(os.path.join('./README.rst'), encoding='utf-8') as f:
     long_desc = f.read()
 setup(
-	name="jhlangtool", 
-	version="0.8.0", 
-	author="Martin Vrbovčan",
-	description="Program that converts jhipster language-specific JSON files into an excel file and back",
-	packages=find_packages(), 
-	long_description=long_desc,
-	long_description_content_type='text/x-rst',
-	install_requires=['xlsxwriter>=1.0.3', 'pandas'],
-	python_requires='>=3',
-	entry_points = {
+    name="jhlangtool",
+    version="0.8.1",
+    url="https://github.com/MartinVrbovcan/jhlangtool",
+    author="Martin Vrbovčan",
+    description="Program that converts jhipster language-specific JSON files into an excel file and back",
+    packages=find_packages(),
+    long_description=long_desc,
+    long_description_content_type='text/x-rst',
+    install_requires=['xlsxwriter>=1.0.3', 'pandas'],
+    python_requires='>=3',
+    entry_points = {
         'console_scripts': ['jhlangtool=jhlangtool.entrypoint:main'],
     },
-	include_package_data=True
-	)
+    include_package_data=True
+)
